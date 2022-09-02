2e5f79ac1dc119e25203d3cfe5231df14ea31932
Néstor Arias <narias@publicissapient.com>
2022-09-02 12:48:53 -0500

    Delete Big 10000 test case

--- clientMain.js
+++ clientMain.js
@@ -23,3 +23,22 @@ async function runTest(e) {
   });
   console.log("🤑 res", res.length)
 }
+
+
+/** @type {NodeListOf<HTMLButtonElement>} */
+const bigTestButton = document.querySelectorAll(".run-big-test");
+bigTestButton.forEach(b => b.addEventListener("click", runBigTest));
+
+async function loadIntArrayTestCase() {
+  const inputJson = await (await fetch("/fixtures/10_000/input.json.bin")).json();
+  const outputJson = await (await fetch("/fixtures/10_000/output.json.bin")).json();
+
+  const input = createCMatrix(inputJson);
+  const output = createArray(outputJson);
+  return { input, output };
+}
+
+async function runBigTest(e) {
+  const {input, output} = await loadIntArrayTestCase();
+  console.log({input, output})
+}
